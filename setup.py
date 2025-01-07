d37040b6371753414676905ca8e259331447393a
Jhen <zhenjhen192@gmail.com>
2025-01-07 14:25:19 -0600

    adding requirements and setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyphen_e_dot = '-e .' # constant for if statment below
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This will return a list of requirements given the input file_path
+    '''
+    requirements = [] # empty list
+    with open(file_path) as file_obj: # open the file
+        requirements = file_obj.readlines() # go through each str and put into a temp file object
+        requirements = [req.replace("\n", " ") for req in requirements] # list comprehension, to replace \n bc of readline
+
+        if hyphen_e_dot in requirements: # additional condition to remove '-e .' in requirements
+            requirements.remove(hyphen_e_dot)
+    
+    return requirements
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
